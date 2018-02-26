1bc5b72cf246dab0488e95391a0a044979e839e5
max7098 <max.mesirow@gmail.com>
2018-02-26 03:00:40 -0500

    implemented the bill array system for the hsm

--- atm_backend/atm_backend/interface/hsm.py
+++ atm_backend/atm_backend/interface/hsm.py
@@ -22,6 +22,10 @@ class HSM(Psoc):
         while not self.connected and not dummy:
             time.sleep(2)
         self._vp('Initialized')
+        self.RETURN_WITHDRAWAL      = 0x09
+        self.RETURN_BALANCE         = 0x0B
+        self.ACCEPTED               = 0x20
+        self.REJECTED               = 0x21
 
     def _authenticate(self, uuid):
         """Requests authentication from the HSM
@@ -54,8 +58,7 @@ class HSM(Psoc):
     def get_nonce(self,transaction):
         self._sync(True)
         self._push_msg(struct.pack('b',transaction))
-        resp = self._pull_msg()
-
+        resp = self.read(size=33) #read 33 bytes
         return struct.unpack('b32s',resp)[1]
         
 
@@ -69,7 +72,8 @@ class HSM(Psoc):
         """
         self._sync(True)
         self._push_msg(struct.pack('b',transaction))
-        uuid = struct.unpack('b32s',self._pull_msg())[1]
+        resp = self.read(size=33) #read 33 bytes
+        uuid = struct.unpack('b32s',resp)[1]
         return uuid
 
     '''
@@ -83,8 +87,31 @@ class HSM(Psoc):
     '''
     def send_action(self,transaction,encrypted_data):
         self._push_msg(signed_nonce_and_amount)
-        resp = self._pull_msg()
-        return resp
+        resp = self.read(size=1)
+        responseAction = struct.unpack('b',resp)
+        if responseAction == self.RETURN_BALANCE: #handles case depending on byte
+            resp = self.read(size=1) #determine if request was bad and should keep reading
+            acceptByte =struct.unpack('b',resp)
+            if acceptByte==self.ACCEPTED:
+                resp = self.read(size=4)
+                balance = struct.unpack('i',resp)
+                return balance #reveals acount balance to atm
+            elif acceptByte==self.REJECTED:
+                raise Exception('HSM Rejected request')
+            else:
+                raise ValueError('Unexpected Byte read from hsm (expected ACCEPT or REJECT byte) got: ' + str(acceptBytes))
+        elif responseAction == self.RETURN_WITHDRAWAL:
+            resp = self.read(size=4)
+            numBills = struct.unpack('i',resp)#number of bills to pull
+            bills = [] #array of bills
+            for i in range(numBills):
+                resp = self.read(size=16)
+                bill = struct.unpack('16s',resp) #loads the 16 byte bill
+                bills.append(bill)
+            return bills
+        else:
+            raise ValueError('Unexpected Byte read from hsm (expected Withdrawal or Check Balance byte ) got: ' + str(responseAction))
+
 
     def provision(self, uuid, bills):
         """Attempts to provision HSM
