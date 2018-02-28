bf71c5bdc5aead84da720d9226f4886540b64fde
Hanson Duan <duanhanson@gmail.com>
2018-02-27 20:12:40 -0500

    added  verify

--- wrapper/cryptotest.py
+++ wrapper/cryptotest.py
@@ -1,5 +1,5 @@
 import crypto
-from binascii import hexlify
+from binascii import hexlify, unhexlify
 
 #sanity check that key is 32 bytes/context is 8 bytes in python if ncessary since it doesnt' work with how the arguments get parsed in C for some reason.
 
@@ -20,3 +20,13 @@ print(len(m1))
 m2 = crypto.secretbox_decrypt(c2, 0, "\x00"*8, '\x00'*32)
 print(m2)
 print(hexlify(m2))
+
+sig = "3a77616ed36fdedbe63e26b9d366a5264b9c61022e8f65ddd6bf4d33ecd87f21e4799ee520f49dc140720b914ec25eafb0fb9e3da1a4327bdd9ad2954feb870b"
+sig = unhexlify(sig)
+m = "\x00essage"
+ctx = "\x00"*8
+pk = "b14ed7aa48d41efc873dcddb33c97a0d5f059b0592597b64278d6b305b889351" 
+pk = unhexlify(pk)
+print(len(pk))
+
+crypto.sign_verify(sig, m1, ctx, pk)
--- wrapper/cryptowrapper.c
+++ wrapper/cryptowrapper.c
@@ -1,3 +1,5 @@
+#define PY_SSIZE_T_CLEAN
+
 #include <Python.h>
 #include <stdio.h>
 #include "../libhydrogen/hydrogen.h"
@@ -11,10 +13,10 @@ static PyObject* crypto_secretbox_encrypt(PyObject* self, PyObject* args){
 	size_t ctx_len;
 	const uint8_t *key; //this shouldn't matter its secret
 	size_t key_len;
-	PyArg_ParseTuple(args, "s#Ks#s#", &m, &mlen, &msg_id, &ctx, &ctx_len, &key, &key_len); //only s# accepts null bytes, need to random pointers to store lengths
+	PyArg_ParseTuple(args, "s#|K|s#|s#", &m, &mlen, &msg_id, &ctx, &ctx_len, &key, &key_len); //only s# accepts null bytes, need to random pointers to store lengths
 	//printf("Parsed \n");
 	//printf("MSG: %s, MSG_ID: %" PRIu64 ", CTX: %s\n", m, msg_id, ctx);
-	/* idk why it parses the message length correctly but not the context length or key length, whatever, ignore those variables
+	/* idk why it parses the message length correctly but not the context length or key length, whatever, ignore those variables*/
 	if(ctx_len != hydro_secretbox_CONTEXTBYTES){
 		PyErr_Format(PyExc_ValueError, "Context not of correct size: Received %lu bytes", ctx_len);
 		return NULL;
@@ -24,7 +26,6 @@ static PyObject* crypto_secretbox_encrypt(PyObject* self, PyObject* args){
 		PyErr_Format(PyExc_ValueError, "Key not of correct size: Received %lu bytes", key_len);
 		return NULL;
 	}
-	*/
 
 	//be careful adding print debug statements, i randomly get segfaults/get unintended functionality
 
@@ -39,7 +40,7 @@ static PyObject* crypto_secretbox_encrypt(PyObject* self, PyObject* args){
 	//printf("\n");
 
 
-	PyObject *ret = Py_BuildValue("s#", c); //s# incase it has null bytes?
+	PyObject *ret = Py_BuildValue("s#", c, hydro_secretbox_HEADERBYTES + mlen); //s# incase it has null bytes?
 	return ret;
 }
 static PyObject* crypto_secretbox_decrypt(PyObject* self, PyObject* args){
@@ -51,26 +52,65 @@ static PyObject* crypto_secretbox_decrypt(PyObject* self, PyObject* args){
 	uint8_t *key;
 	size_t key_len;
 	PyArg_ParseTuple(args, "s#Ks#s#", &c, &clen, &msg_id, &ctx, &ctx_len, &key, &key_len);
+	if(ctx_len != hydro_secretbox_CONTEXTBYTES){
+		PyErr_Format(PyExc_ValueError, "Context not of correct size: Received %lu bytes", ctx_len);
+		return NULL;
+	}
+
+	if(key_len != hydro_secretbox_KEYBYTES){
+		PyErr_Format(PyExc_ValueError, "Key not of correct size: Received %lu bytes", key_len);
+		return NULL;
+	}
 
 	char m[clen - hydro_secretbox_HEADERBYTES];
-	if (hydro_secretbox_decrypt(m, c, clen, msg_id, ctx, key) != 0){ //when returning to python, two bytes get appended, idk why but remove those end bytes, leaving this comment here but it doesn't happen anymore for some reason
+	if (hydro_secretbox_decrypt(m, c, clen, msg_id, ctx, key) != 0){ 
 		PyErr_Format(PyExc_ValueError, "Message forged: %s, did not decrypt successfully?\n", m);
 		return NULL;
 	}
-	//don't comment this out, it segfaults, idk why
-	for (int x = 0; x < clen - hydro_secretbox_HEADERBYTES; x++){
-		//printf("%c", m[x]);
-	}
-	PyObject *ret = Py_BuildValue("s#", m);
+	PyObject *ret = Py_BuildValue("s#", m, clen-hydro_secretbox_HEADERBYTES);
 	return ret;
+}
+
+static PyObject* crypto_sign_verify(PyObject* self, PyObject* args){
+	uint8_t* csig;
+	size_t siglen;
+	char* m;
+	size_t mlen;
+	char* ctx;
+	size_t ctxlen;
+	uint8_t* pk;
+	size_t pklen;
+	PyArg_ParseTuple(args, "s#|s#|s#|s#", &csig, &siglen, &m, &mlen, &ctx, &ctxlen, &pk, &pklen);
+	if (ctxlen != hydro_sign_CONTEXTBYTES){
+		PyErr_Format(PyExc_ValueError, "Context not of correct size: Received %lu bytes", ctxlen);
+		return NULL;
+	}
+
+	if(pklen != hydro_sign_PUBLICKEYBYTES){
+		PyErr_Format(PyExc_ValueError, "Public key not of correct size: Received %lu bytes", pklen);
+		return NULL;
+
+	}
+
+
+	if(hydro_sign_verify(csig, m, mlen, ctx, pk) != 0){
+		PyErr_Format(PyExc_ValueError, "Message failed to verify\n");
+		return NULL;
+	}
+	
+	Py_INCREF(Py_None);
+	return Py_None;
+
 }
 
 static char module_docstring[] = "This module provides an interface for several libhydrogen functions";
 static char secretbox_encrypt_docstring[] = "Encrypts a message of length mlen, using a context and secret key, with message counter msg_id";
 static char secretbox_decrypt_docstring[] = "Decrypts ciphertext, using the message id, context, and secret key";
+static char sign_verify_docstring[] = "Checks if message m verifies with signature csig, with context ctx and publike key pk";
 static PyMethodDef module_methods[] = {
 	{"secretbox_encrypt", crypto_secretbox_encrypt, METH_VARARGS, secretbox_encrypt_docstring},
-	{"secretbox_decrypt", crypto_secretbox_decrypt, METH_VARARGS, secretbox_decrypt_docstring}
+	{"secretbox_decrypt", crypto_secretbox_decrypt, METH_VARARGS, secretbox_decrypt_docstring},
+	{"sign_verify", crypto_sign_verify, METH_VARARGS, sign_verify_docstring}
 };
 
 PyMODINIT_FUNC initcrypto(void){
